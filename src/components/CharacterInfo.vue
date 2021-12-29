2c9c4d13564e0611d00f7285a99a86f9c2c8e649
EsterAndersson <estermollyandersson@hotmail.com>
2021-12-29 22:35:30 +0100

    added another label och some design

--- src/components/CharacterInfo.vue
+++ src/components/CharacterInfo.vue
@@ -1,51 +1,50 @@
-<template>
-  <Page>
-    <ActionBar title="NativeScript-Vue Pokemon"/>
-    <StackLayout backgroundColor="#FFF">
-      <FlexboxLayout flexDirection="column" class="CharacterContainer">
-
-        <Image :src="this.image" />
-        <Label :text="this.character" class="CharacterName" />
-        <Label :text="this.interpretedBy" class="InterpretedBy" />
-        <Label :text="this.hogwartsHouse" class="HogwartsHouse" />
-
-        <Button text="Back" @tap="goBack" />
-
-      </FlexboxLayout>
-    </StackLayout>
-  </Page>
-</template>
-
 <script>
 import App from './App'
 export default {
 
-  props: ['id', 'character', 'image','interpretedBy', 'hogwartsHouse'],
+  props: ['id', 'character', 'image','interpretedBy', 'hogwartsHouse', 'child'],
 
   data() {
     return {
       App : App,
+
     }
   },
 
-    
-
-methods: {
-     goBack: function (args) {
-       this.$navigateBack(App);
-     }
-}
+  methods: {
+      goBack: function (args) {
+        this.$navigateBack(App);
+      }
+  }
 }
 </script>
 
 <style scoped>
 ActionBar {
-  background-color: #53ba82;
+  background-color: #3e81b8;
   color: #ffffff;
 }
 .CharacterContainer {
   align-items: center;
+  background-color: #cacaca;
+  padding: 20, 20, 20, 20;
+  margin: 3%;
+  border-radius: 0, 20, 0, 20;
+  text-shadow: 10, 10, 10, 10 rgb(124, 38, 38);
 }
+.button {
+  background-color: #000000;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  margin:1%;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 0, 20, 0, 20;
+}
+
 Image {
   width: 270;
   margin-top: 20;
@@ -58,4 +57,24 @@ Image {
   padding: 10;
   font-size: 16;
 }
+Label{
+  overflow-wrap: break-word;
+  word-wrap: true;
+}
 </style>
+
+<template>
+  <Page>
+    <ActionBar :title="this.character"/>
+      <FlexboxLayout flexDirection="column" class="CharacterContainer" >
+        <Image :src="this.image" />
+        <Label :text="this.character" class="CharacterName" />
+        <Label :text="this.interpretedBy" class="InterpretedBy" />
+        <Label :text="this.hogwartsHouse" class="HogwartsHouse" />
+        <Label :text="this.child" class="Child" />
+
+        <Button text="Back" @tap="goBack" class="button"/>
+
+      </FlexboxLayout>
+  </Page>
+</template>
